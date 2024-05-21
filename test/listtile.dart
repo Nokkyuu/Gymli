1a26bb01d966fdf62b8465f9313c8dc91bc1f773
Nokkyuu <nils.ayral@alum.uol.de>
2024-05-21 23:13:24 +0200

    workout setup screen

--- test/listtile.dart
+++ test/listtile.dart
@@ -34,17 +34,37 @@ class _ListTileExampleState extends State<ListTileExample> {
     return Scaffold(
       appBar: AppBar(title: const Text('ListTile Sample')),
       body: Center(
-        child: ListTile(
-          enabled: true,
-          leading: const Icon(Icons.sports_tennis),
-          title: Text(exerciseName),
-          subtitle: Text('Warm Ups: $warmUpS, Work Sets: $workS, Drop Sets: $dropS'),
-          trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed:  () => print("deleted")
-                      ),
-        ),
+        child: ExerciseTile(exerciseName: exerciseName, warmUpS: warmUpS, workS: workS, dropS: dropS),
       ),
+    );
+  }
+}
+
+class ExerciseTile extends StatelessWidget {
+  const ExerciseTile({
+    super.key,
+    required this.exerciseName,
+    required this.warmUpS,
+    required this.workS,
+    required this.dropS,
+  });
+
+  final String exerciseName;
+  final int warmUpS;
+  final int workS;
+  final int dropS;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      enabled: true,
+      leading: const Icon(Icons.sports_tennis),
+      title: Text(exerciseName),
+      subtitle: Text('Warm Ups: $warmUpS, Work Sets: $workS, Drop Sets: $dropS'),
+      trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed:  () => print("deleted")
+                  ),
     );
   }
 }
