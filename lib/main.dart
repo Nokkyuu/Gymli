1630edace70203a25abd08d43834136a36039d32
Nokkyuu <nils.ayral@alum.uol.de>
2024-05-22 14:53:21 +0200

    tiny bit cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'dart:math';
-//import 'package:fl_chart/fl_chart.dart';
-//import 'package:yafa_app/exerciseListScreen.dart';
-//import 'package:yafa_app/exerciseScreen.dart';
 import 'package:yafa_app/landingScreen.dart';
-//import 'package:yafa_app/exerciseSetupScreen.dart';
 
 void main() {
   runApp(const MaterialApp(
