2c26c753b64553a18236f9ca7a384f955b9c1676
jbrgmnn <jim.bergmann@tu-dortmund.de>
2024-05-27 20:33:48 +0200

    Added exercise list with removal feature

--- lib/exerciseListScreen.dart
+++ lib/exerciseListScreen.dart
@@ -2,9 +2,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:yafa_app/DataModels.dart';
 
 final workIcons = [FontAwesomeIcons.fire, FontAwesomeIcons.handFist, FontAwesomeIcons.arrowDown];
 class ExerciseListScreen extends StatelessWidget {
+
+  static String exercise = "Benchpress";
   static List<ListItem> items = [
     DateItem("10.02.2024"),
     ExerciseItem('10 kg for 6 Reps', '11:42:21', workIcons.first),
@@ -35,18 +39,31 @@ class ExerciseListScreen extends StatelessWidget {
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
           Expanded(
-              child: ListView.builder(
-              itemCount: items.length,
-              // Provide a builder function. This is where the magic happens.
-              itemBuilder: (context, index) {
-                final item = items[index];
-                return ListTile(
-                  tileColor: item.getColor(context),
-                  leading: item.buildIcon(context),
-                  title: item.buildTitle(context),
-                  subtitle: item.buildSubtitle(context),
-                  trailing: item.buildTrailing(context), 
-                );
+            child: ValueListenableBuilder(
+              valueListenable: Hive.box<TrainingSet>('TrainingSets').listenable(),
+              builder: (context, Box<TrainingSet> box, _) {
+                var items = box.values.toList();
+                items = box.values.where((item) => item.exercise == exercise).toList().reversed.toList();
+                if (items.isNotEmpty) {
+                  return ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      final item = items[index];
+                      return ListTile(
+                        leading: CircleAvatar(radius: 17.5, child: FaIcon(workIcons[item.setType])),
+                        title: Text("${item.weight}kg for ${item.repetitions} reps"),
+                        subtitle: Text("${item.date}"),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed:  () =>  {
+                            box.delete(item.key)
+                            // print("Delete funzt nicht.")
+                          })
+                        );
+                  });
+                } else {
+                  return Text("None");
+                }
               })
             )
           ]
