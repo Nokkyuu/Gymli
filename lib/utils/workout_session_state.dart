131921400e40b88024e3e27f4114718dae4294ba
jbrgmnn <jim.bergmann@tu-dortmund.de>
2025-08-20 14:37:13 +0200

    Workoutsessionstate

---
+++ lib/utils/workout_session_state.dart
@@ -0,0 +1,40 @@
+/// State Management for Workout-Sessions
+/// TTL should be adapted
+///
+
+class WorkoutSessionState {
+  late DateTime startTime;
+  late DateTime lastExerciseTime;
+  List<String> completedExercises = [];
+  final Duration ttl; // Time to Live (z. B. 2 Stunden)
+
+  WorkoutSessionState._({
+    required this.startTime,
+    required this.ttl,
+  });
+
+  factory WorkoutSessionState.startNew({Duration ttl = const Duration(hours: 2)}) {
+    final now = DateTime.now();
+    return WorkoutSessionState._(startTime: DateTime.now(), ttl: ttl)..lastExerciseTime = now;
+  }
+
+  bool get isActive => DateTime.now().difference(startTime) < ttl;
+  Duration get timeLeft => ttl - DateTime.now().difference(startTime);
+
+  void addExercise(String name) {
+    completedExercises.add(name);
+    lastExerciseTime = DateTime.now();
+  }
+}
+
+class WorkoutSessionManager {
+  WorkoutSessionState? _session;
+
+  WorkoutSessionState getSession() {
+    if (_session == null || !_session!.isActive) {
+      _session = WorkoutSessionState.startNew();
+    }
+    return _session!;
+  }
+  WorkoutSessionState? get session => _session;
+}
